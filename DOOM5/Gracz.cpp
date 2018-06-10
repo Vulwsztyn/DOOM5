c9874b6dc7f3fddc577b90264019192e593f7933
Luker <luker.plus@gmail.com>
2018-06-10 19:56:25 +0200

    opis

--- DOOM5/Gracz.cpp
+++ DOOM5/Gracz.cpp
@@ -47,12 +47,12 @@ void Gracz::rusz(double czas)
 
 }
 
-float Gracz::sign(glm::vec2 p1, glm::vec2 p2, glm::vec2 p3)
+float Gracz::sign(glm::vec2 p1, glm::vec2 p2, glm::vec2 p3)  // po której stronie prostej jest punkt
 {
 	return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
 }
 
-bool Gracz::PointInTriangle(glm::vec2 position, glm::vec2 triangle[3])
+bool Gracz::PointInTriangle(glm::vec2 position, glm::vec2 triangle[3]) //funkcja wykrywaj¹ca czy punkt jest w obrêbie trójk¹ta spatrzona z neta, wiêc chyba dobra
 {
 	bool b1, b2, b3;
 
@@ -63,10 +63,10 @@ bool Gracz::PointInTriangle(glm::vec2 position, glm::vec2 triangle[3])
 	return ((b1 == b2) && (b2 == b3));
 }
 
-bool Gracz::detectTerrainColision(Model &map,bool debug) {
+bool Gracz::detectTerrainColision(Model &map,bool debug) { // detekcja kolizji z terenem narazie daje 1 jak jest w obrêbie jakiegoœ trójk¹ta i 0 jak nie jest patrz¹c tylko na x i z
 	glm::vec3 triangle[3];
-	for(int i = 0; i < map.getVertices().size(); i = i + 12) {
-		triangle[0].x = map.getVertices()[i];
+	for(int i = 0; i < map.getVertices().size(); i = i + 12) { //huh, no to tak, te Vertices s¹ zapisane po kolei jako x,y,z i ten mno¿nik co jest zawsze jeden na razie
+		triangle[0].x = map.getVertices()[i];                  //wiêc zczytujemy tylko x,y,z 3 punktów tworz¹cych trójk¹t
 		triangle[0].y = map.getVertices()[i+1];
 		triangle[0].z = map.getVertices()[i+2];
 
@@ -76,12 +76,12 @@ bool Gracz::detectTerrainColision(Model &map,bool debug) {
 
 		triangle[2].x = map.getVertices()[i+8];
 		triangle[2].y = map.getVertices()[i+8 + 1];
-		triangle[2].z = map.getVertices()[i+8 + 2];
+		triangle[2].z = map.getVertices()[i+8 + 2];           
 
 		glm::vec2 planeTriangle[3] = { glm::vec2(triangle[0].x,triangle[0].z),glm::vec2(triangle[1].x,triangle[1].z) ,glm::vec2(triangle[2].x,triangle[2].z) };
-		//glm::vec2 planeTriangle[3] = { glm::vec2(-5,5),glm::vec2(5,5) ,glm::vec2(0,0) };
-		if (PointInTriangle(glm::vec2(position.x, position.z), planeTriangle)) {
-			if (debug == true) {
+
+		if (PointInTriangle(glm::vec2(position.x, position.z), planeTriangle)) {  //po wczytaniu takiego trójk¹ta sprawdzam czy pozycja gracza jest w jego wnêtrzu
+			if (debug == true) {                                                  // wypisuje x i z tego trójk¹ta w którym rzekomo jest gracz
 				std::cout << position.x << " " << position.y << std::endl;
 				std::cout << triangle[0].x << " " << triangle[0].z << " " << triangle[1].x << " " << triangle[1].z << " " << triangle[2].x << " " << triangle[2].z << std::endl;
 			}
